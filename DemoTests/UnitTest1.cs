fae8b40d92eafc29f241f31933911841a3396b61
Shawn <unshorn@gmail.com>
2025-04-07 11:12:45 +1200

    added a test

--- DemoTests/UnitTest1.cs
+++ DemoTests/UnitTest1.cs
@@ -1,10 +1,24 @@
 namespace DemoTests;
 
+using BookLibraryApp.Models;
+
 public class UnitTest1
 {
     [Fact]
     public void Test1()
     {
+        MyLinkedList<Book> list = new MyLinkedList<Book>();
+        var book = new Book
+                {
+                    Title = "Beloved",
+                    Author = "Toni Morrison",
+                    Year = 1987
+                };
+
+        list.Add(book);
+        var result = list.Find(b => b.Title.ToLower() == "beloved");
+        Assert.NotNull(result);
+
 
     }
 }
